f206b06395fdf3a89a9a06d56f95420aaf64b66e
stephane delatorre <s.dlt@hotmail.fr>
2017-12-25 10:16:35 +0100

    session 4/28 done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+require("./services/passport");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+
+require("./routes/authRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
