01151058f76b4863748945525d3d63da62c6b406
stephane delatorre <s.dlt@hotmail.fr>
2017-12-30 18:07:39 +0100

    session 4/36 done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
-require("./services/passport");
+
 require("./models/user");
+require("./services/passport");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 require("./routes/authRoutes")(app);
 
 mongoose.connect(keys.mongoURI);
