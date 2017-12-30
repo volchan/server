c3a6928de0cd6de62e9a3c7fbd42d3a89eea2667
stephane delatorre <s.dlt@hotmail.fr>
2017-12-30 17:50:11 +0100

    session 4/35 done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 require("./services/passport");
+require("./models/user");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
