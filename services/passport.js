eb21bce3111a21fec3f413293214d4a946a2cf1f
stephane delatorre <s.dlt@hotmail.fr>
2017-12-30 18:26:09 +0100

    session 4/38 done

--- services/passport.js
+++ services/passport.js
@@ -15,9 +15,11 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleID: profile.id }).then(existingUser => {
         if (existingUser) {
-          
+          done(null, existingUser);
         } else {
-          new User({ googleID: profile.id }).save();
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
