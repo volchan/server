347c0b8b842f31087409e84dedecce14042100b5
stephane delatorre <s.dlt@hotmail.fr>
2018-01-01 15:57:03 +0100

    session 4/43 done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 
 require("./models/user");
@@ -7,6 +9,16 @@ require("./services/passport");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
