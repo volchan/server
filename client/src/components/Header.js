34590c0a057537986f1fe13cb1a74e41d9c9bcf0
stephane delatorre <s.dlt@hotmail.fr>
2018-01-14 21:11:26 +0100

    session 7/81 done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Header extends Component {
-
-    render() {
-        return (
-            <nav className="light-blue accent-4">
-              <div className="nav-wrapper">
-                <a href="/" className="left brand-logo">
-                  Emaily
-                </a>
-                <ul className="right">
-                  <li>
-                    <a href="/auth/google">Login With Google</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-        );
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "Still waiting...";
+      case false:
+        return "I'm Logged out!";
+      default:
+        return "I'm Logged in!";
     }
+  }
+
+  render() {
+    return (
+      <nav className="light-blue accent-4">
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
+            Emaily
+          </a>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
