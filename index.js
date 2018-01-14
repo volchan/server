0d0f919677ff8b299b4c2a2a34f90bb105388f22
stephane delatorre <s.dlt@hotmail.fr>
2018-01-14 15:30:37 +0100

    session 6/53 done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("./models/user");
 require("./services/passport");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cookieSession({
