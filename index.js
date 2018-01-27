b2005adc4899bf7e75aedcf0428eaea7c8531be9
stephane de la torre <s.dlt@hotmail.fr>
2018-01-27 19:20:23 +0100

    session 8/100 done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
 require("./models/user");
@@ -10,6 +11,7 @@ require("./services/passport");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 2592000000,
