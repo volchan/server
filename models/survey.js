e088c678ec2de4d17e0d825d92217282b2720909
stephane de la torre <s.dlt@hotmail.fr>
2018-01-28 00:18:18 +0100

    session 10/119 done

--- models/survey.js
+++ models/survey.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const RecipientSchema = require("./Recipient")
+const RecipientSchema = require("./Recipient");
 
 const surveySchema = new Schema({
   title: String,
   subject: String,
   body: String,
   recipients: [RecipientSchema],
-  yes: {type: Number, default: 0},
-  no: {type: Number, default: 0}
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);
