d9e7367d4a09d978c9333476564ae7773d858084
stephane de la torre <s.dlt@hotmail.fr>
2018-01-28 18:40:12 +0100

    session 10/134 done

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
+import axios from "axios";
 
 import App from "./components/App";
 import reducers from "./reducers";
+
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
