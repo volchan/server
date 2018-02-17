a6d1aee17142816184ee0bbff1999aa96159922b
stephane de la torre <s.dlt@hotmail.fr>
2018-02-17 19:58:06 +0100

    session 10/141 done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large waves-effect waves-light light-blue accent-4">
+        <Link
+          to={"/surveys/new"}
+          className="btn-floating btn-large waves-effect waves-light light-blue accent-4"
+        >
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>
   );
