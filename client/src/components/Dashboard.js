9202f2e6f916c00c0309035fe319f6563d633da7
stephane de la torre <s.dlt@hotmail.fr>
2018-02-17 19:55:31 +0100

    session 10/140 done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large waves-effect waves-light light-blue accent-4">
+          <i className="material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
