bc1a93914c4dd0dc36c4f208e8c063eb36f539aa
stephane de la torre <s.dlt@hotmail.fr>
2018-02-18 18:35:18 +0100

    session 10/153 done

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-export default ({ input, label }) => {
+export default ({ input, label, meta }) => {
+  const { error, touched } = meta;
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
