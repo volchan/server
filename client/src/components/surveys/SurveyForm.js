1a84ebb6555b05fc5462f85a473252d15756f5b9
stephane de la torre <s.dlt@hotmail.fr>
2018-02-18 18:26:35 +0100

    session 10/152 done

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -57,4 +57,14 @@ class SurveyForm extends Component {
   }
 }
 
-export default reduxForm({ form: "surveyForm" })(SurveyForm);
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "You must privide a title";
+  }
+
+  return errors;
+};
+
+export default reduxForm({ validate, form: "surveyForm" })(SurveyForm);
