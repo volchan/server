55d4bde86f06c73243ab9162694f980f0527d1d1
stephane de la torre <s.dlt@hotmail.fr>
2018-03-10 19:02:41 +0100

    session 11/165

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
@@ -8,9 +9,11 @@ class SurveyNew extends Component {
 
   renderContent() {
     if (this.state.showFormReview) {
-      return <SurveyFormReview
-        onCancel={() => this.setState({ showFormReview: false })}
-      />;
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
     }
 
     return (
@@ -25,4 +28,6 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyNew);
