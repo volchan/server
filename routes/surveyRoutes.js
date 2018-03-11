cdf8e3e3bbade0cbbebbd77cbde503e0177fdc77
stephane de la torre <s.dlt@hotmail.fr>
2018-03-11 17:32:58 +0100

    session 11/189

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -16,7 +16,9 @@ module.exports = app => {
   });
 
   app.get("/api/surveys", requireLogin, async (req, res) => {
-    const surveys = await Survey.find({ _user: req.user.id });
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
 
     res.send(surveys);
   });
